e4b2c4e417b7112e2ec0d5f8e671ebccf64b221b
紺野由夏 <@y_konno>
2024-07-16 16:55:13 +0900

    課題08　完了

---
+++ src/hierarchy/life/Flower.java
@@ -0,0 +1,26 @@
+package hierarchy.life;
+
+/**
+ * 植物を表す抽象クラス(Plant)を継承した新たな生物を作成
+ * @author 紺野由夏
+ *
+ */
+public class Flower extends Plant {
+	public Flower(int lifespan) {
+		super(lifespan);
+	}
+
+	void toAct() {
+		// 乱数で動作を決める(1～3:花を咲かせて種子を残す、それ以外:何もしない)
+		int number = getRandomNumber();
+
+		if (3 >= number) {
+			Earth.getInstance().birthCreature(new Flower(2));
+			Earth.getInstance().birthCreature(new Flower(2));
+			Earth.getInstance().birthCreature(new Flower(2));
+			System.out.println(this.getClass().getSimpleName() + "は花を咲かせて種子を残す");
+		} else {
+			System.out.println(this.getClass().getSimpleName() + "は何もしない");
+		}
+	}
+}
